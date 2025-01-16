da1f6e8127856534140e7d97bb0880f13defec0a
Fahleen1 <mishaarif14@gmail.com>
2025-01-16 11:54:12 +0500

    Enhance backend setup with database connection, initialization script, and default image handling for listings

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,10 +1,12 @@
 import dotenv from 'dotenv';
 
 import { app } from './app';
+import { connectToDb } from './config/mongoose';
 
 dotenv.config();
 const port: number = parseInt(process.env.BACKEND_PORT || '3002', 10);
 
+connectToDb();
 app.listen(port, () => {
   console.log(`App is listening on port ${port}`);
 });
--- apps/backend/src/models/listings.ts
+++ apps/backend/src/models/listings.ts
@@ -13,6 +13,10 @@ const listingsSchema = new mongoose.Schema({
     filename: String,
     url: {
       type: String,
+      set: (v: string) =>
+        v === ''
+          ? 'https://images.unsplash.com/photo-1501785888041-af3ef285b470?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHRyYXZlbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60'
+          : v,
     },
   },
   price: {
@@ -26,3 +30,5 @@ const listingsSchema = new mongoose.Schema({
     type: String,
   },
 });
+
+export const Listings = mongoose.model('listings', listingsSchema);
